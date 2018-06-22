93db711589a2f5869ce2ae5344320f91e5a54ae7
mertcelil <mertcelilozdin@hotmail.com>
2018-06-23 00:35:29 +0300

    Elementlerin DOM seçimi yapıldı; Kullanıcının girdiği genişlik,uzunluk,renk değişken içine atılabildi

--- designs.js
+++ designs.js
@@ -1,10 +1,31 @@
 // Select color input
 // Select size input
+const color = document.querySelector("#colorPicker");
+const height = document.querySelector("#inputHeight");
+const weight = document.querySelector("#inputWeight");
+const submit = document.querySelector("input[type=submit]");
+  let colorValue;
+  let heightValue;
+  let weightValue;
 
+submit.addEventListener("click",function (evt){
+  evt.preventDefault();
+  colorValue = color.value;
+  // color.setAttribute("value",color);
+
+  heightValue = height.value;
+  // color.setAttribute("value",height);
+
+  weightValue = weight.value;
+  // color.setAttribute("value",weight);
+  makeGrid();
+});
+//submit e click ile event koy
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
 
+  console.log(colorValue,weightValue,heightValue);
 // Your code goes here!
 
 }
