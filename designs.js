352a1c9c9de823e9f0fd281e66495e087422edab
Mert Celil <32895330+tufanlodos@users.noreply.github.com>
2019-03-12 00:57:36 +0300

    sayfanın kod tarafındaki özelliklerden bazıları güncellendi

--- designs.js
+++ designs.js
@@ -1,30 +1,55 @@
-// Select color input
-// Select size input
-const color = document.querySelector("#colorPicker");
-const height = document.querySelector("#inputHeight");
-const weight = document.querySelector("#inputWeight");
+const color = document.querySelector("#color-picker");
+const height = document.querySelector("#input-height");
+const weight = document.querySelector("#input-weight");
 const submit = document.querySelector("input[type=submit]");
-const table = document.querySelector("#pixelCanvas");
-const click = document.querySelectorAll('#pixelCanvas td');
-  let colorValue;
-  let heightValue;
-  let weightValue;
+const table = document.querySelector("#pixel-canvas");
+const allTds = table.getElementsByTagName('TD');
+const allTrs = table.getElementsByTagName('tr');
+const clear = document.querySelector("#clear-canvas");
+let colorValue = "black";
+let heightValue;
+let weightValue;
 
+//events
 submit.addEventListener("click",function (evt){
   evt.preventDefault();
-  colorValue = color.value;
-  // color.setAttribute("value",color);
-
+  for (let l = 0; l < allTds.length; l++) {
+    allTds[l].remove();
+    for (let k = 0; k < allTrs.length; k++) {
+      allTrs[k].remove();
+    };
+  };
+  
   heightValue = height.value;
-  // color.setAttribute("value",height);
-
   weightValue = weight.value;
-  // color.setAttribute("value",weight);
   makeGrid();
 });
 
-// When size is submitted by the user, call makeGrid()
+table.addEventListener('click', function (evt) {
+  if (evt.target.nodeName === 'TD') {  // ← verifies target is desired element
+    // set the cell to the picked color
+    evt.target.bgColor = colorValue; //evt yerine event de yazsak aynı oluyor.
+  }
+});
 
+color.addEventListener("change",function(){
+  colorValue = color.value;
+});
+
+clear.addEventListener("click", function(){
+  // // var alltds = document.getElementsByTagName("td");
+  // // for (var tr in alltds) {
+  // //   tr.bgColor = "white";
+  // // }
+  // document.querySelectorAll('td').bgColor = "white";
+  console.log("gelieer");
+  for (let i = 0; i < allTds.length; i++) {
+    allTds[i].bgColor = "white";
+  }
+  });
+
+
+//functions
 function makeGrid() {
   for (let i = 0; i < heightValue ; i++) {
     const newRow = document.createElement("tr");
@@ -35,11 +60,3 @@ function makeGrid() {
       }
     }
   };
-
-  //click ile cell in background-color ı colorValue olacak
-  document.querySelector('#pixelCanvas').addEventListener('click', function (evt) {
-    if (evt.target.nodeName === 'TD') {  // ← verifies target is desired element
-      // set the cell to the picked color
-      evt.target.bgColor = colorValue; //evt yerine event de yazsak aynı oluyor.
-    }
-});
